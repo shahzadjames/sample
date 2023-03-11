6e2d3ba47e9148bbea0434da88646d4889d8f8e9
shahzadjames <44785418+shahzadjames@users.noreply.github.com>
2023-03-12 01:36:34 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
-{/* <script>
-                function toggleMenu(){    
-                    var mainMenu = document.getElementsByClassName("main-menu")[0];
-                    var icon = document.getElementById("menu-icon");
-                    if (mainMenu.style.display === "block"){
-                        mainMenu.style.display = "none";
-                        icon.className = 'fa fa-bars';
-                    } 
-                    else {
-                        mainMenu.style.display = "block";
-                        icon.className = 'fa fa-close';
-                    }
-                }
-            </script> */}
+
 function toggleMenu(){    
     var mainMenu = document.getElementsByClassName("main-menu")[0];
     var icon = document.getElementById("menu-icon");
@@ -24,3 +11,4 @@ function toggleMenu(){
         icon.className = 'fa fa-close';
     }
 }
+
